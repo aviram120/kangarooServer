2c6cfb7f216e13c9f78e3cf5eb0a97d90ca73c3b
aviram120 <aviram1202@gmail.com>
2017-01-21 22:03:23 +0200

    update user

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.post('/update_user', function (request, response,next) {
 	var userPropertiesJson = JSON.parse(userProperties);
 	var locationJson = JSON.parse(location);
 	
-	var stReturn = '';
+	var stReturn;
 	connectDatabase().query('call update_user( ' + userId + ' , ' + userPropertiesJson['phone'] + ', "' + userPropertiesJson['birthDay'] + '", "' + userPropertiesJson['about'] + '")', function(err, rows, fields) {
 			if (err) {
 				console.log('error: ', err);
@@ -175,10 +175,10 @@ app.post('/update_user', function (request, response,next) {
 			stReturn  = stReturn + '"update_user": "true" ,';
 		});
 		
-	console.log('call add_location( ' + userId + ' , ' + locationJson['country'] + ', ' + locationJson['city'] + ', ' + locationJson['street'] +  ', ' + locationJson['radius'] +  ', ' + locationJson['x'] +  ', ' + locationJson['y'] + ')');
+	console.log('call add_location( ' + userId + ' , "' + locationJson['country'] + '", "' + locationJson['city'] + '", "' + locationJson['street'] +  '", ' + locationJson['radius'] +  ', ' + locationJson['x'] +  ', ' + locationJson['y'] + ')');
 	//`add_location`(IN user_IdIn int, IN countryIn VARCHAR(45), IN cityIn VARCHAR(45),IN streetIn VARCHAR(45), IN radiusIn VARCHAR(45), IN xIn VARCHAR(45), IN yIn VARCHAR(45))
-	/*
-	connectDatabase().query('call add_location( ' + userId + ' , ' + locationJson['country'] + ', ' + locationJson['city'] + ', ' + locationJson['street'] +  ', ' + locationJson['radius'] +  ', ' + locationJson['x'] +  ', ' + locationJson['y'] + ')', function(err, rows, fields) {
+	
+	connectDatabase().query('call add_location( ' + userId + ' , "' + locationJson['country'] + '", "' + locationJson['city'] + '", "' + locationJson['street'] +  '", ' + locationJson['radius'] +  ', ' + locationJson['x'] +  ', ' + locationJson['y'] + ')', function(err, rows, fields) {
 			if (err) {
 				console.log('error: ', err);
 				throw err;
@@ -186,7 +186,7 @@ app.post('/update_user', function (request, response,next) {
 			
 			stReturn  = stReturn + '"add_location": "true" ';
 		});
-	*/	
+		
 	console.log("update_user[response] - " + stReturn);
 	response.json({success:true, data:stReturn });
 });
