94dc5a2e22a6933077962b72fc8655f8873d4027
aviram120 <aviram1202@gmail.com>
2017-01-23 18:29:49 +0200

    json array

--- index.js
+++ index.js
@@ -169,9 +169,8 @@ app.post('/update_user', function (request, response,next) {
 	//var locationJson = JSON.parse(location);
 	
 var arr2 = "{"country":["il", "usa"],"city":["tel aviv", "ny"],"street":["zhal", "inbal"]}";
-//var jStr = JSON.stringify(arr2);
-
-var parsedArr = JSON.parse(arr2);
+var jStr = JSON.stringify(arr2);
+var parsedArr = JSON.parse(jStr);
 
 console.log("sys180: " + parsedArr);
 console.log('call add_location( ' + userId + ' , ' + parsedArr.country + ')');
