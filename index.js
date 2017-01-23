2cb7f0fe6a6771aa541524ae60e9769fdd2f2441
aviram120 <aviram1202@gmail.com>
2017-01-23 19:15:35 +0200

    json array

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.post('/update_user', function (request, response,next) {
 	
 	for (var i = 0; i < locationJson.length; i++)
 	{
-			console.log('add_location' + userId + ' , "' + locationJson.country[i] + '", "' + locationJson.city[i] + '", "' + locationJson.street[i] + '", ' + locationJson.radius[i] + ', ' + locationJson.x[i] +  ', ' locationJson.y[i]  + ')');
+			console.log('add_location' + userId + ' , "' + locationJson.country[i] + '", "' + locationJson.city[i] + '", "' + locationJson.street[i] + '", ' + locationJson.radius[i] + ', ' + locationJson.x[i] +  ', ' + locationJson.y[i]  + ')' );
 	}
 	/*
 	connectDatabase().query('call add_location( ' + userId + ' , "' + locationJson['country'] + '", "' + locationJson['city'] + '", "' + locationJson['street'] +  '", ' + locationJson['radius'] +  ', ' + locationJson['x'] +  ', ' + locationJson['y'] + ')', function(err, rows, fields) {
