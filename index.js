f0c24108c4580c5e6e3821777a811b8869b1f908
aviram120 <aviram1202@gmail.com>
2017-02-06 22:21:22 +0200

    call to store

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
     if ('OPTIONS' == req.method) {
-
+ 
         res.send(200);
     }
     else {
